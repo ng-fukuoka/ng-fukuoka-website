2596a61a394317d543cf26a42cbe885b496e7149
puku0x <puku0x@gmail.com>
2019-10-11 13:51:25 +0900

    add event lint

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const modules = [MatButtonModule, MatCardModule, MatToolbarModule];
+const modules = [
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatToolbarModule
+];
 
 @NgModule({
   imports: [...modules],
