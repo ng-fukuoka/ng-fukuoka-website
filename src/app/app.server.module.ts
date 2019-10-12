8db770698c28792a7744d5ad3f63b17e65eb0004
puku0x <puku0x@gmail.com>
2019-10-12 11:08:00 +0900

    fix app-shell

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { Routes, RouterModule } from '@angular/router';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppShellComponent } from './app-shell/app-shell.component';
 import { AppModule } from './app.module';
@@ -10,12 +9,7 @@ import { AppComponent } from './app.component';
 const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-    MatProgressSpinnerModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [AppModule, ServerModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent]
 })
