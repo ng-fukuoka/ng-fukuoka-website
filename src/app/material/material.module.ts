dc4e983f5ec397932f7a0e862da0dbb25110a513
puku0x <puku0x@gmail.com>
2019-11-02 16:19:06 +0900

    add svg icons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 const modules = [
   MatButtonModule,
   MatCardModule,
+  MatIconModule,
   MatSnackBarModule,
   MatTableModule,
   MatToolbarModule
