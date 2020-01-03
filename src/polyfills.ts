d6b06ae42e331d56bfc78293b5976db52d57361c
puku0x <puku0x@gmail.com>
2020-01-03 14:16:47 +0900

    add scully

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,9 +55,13 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/***************************************************************************************************
+ * SCULLY IMPORTS
+ */
+import 'zone.js/dist/task-tracking';
